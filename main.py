f5cb453619dd47a27d0523fcb0276518f0393756
SuperluminalDreams <jschubert1016@gmail.com>
2022-07-25 13:45:54 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# This is a game about gravity. The player launches a projectile with some initial velocity into a field of bodies; each have some gravity. 
+# The goal of the game is to have the have the projectile orbit the bodies for as long as possible before it exits the screen.
+
