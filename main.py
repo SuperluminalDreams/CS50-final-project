408b3be8a8aefc8cd27e09ee32ddb3634fc12a5f
SuperluminalDreams <jschubert1016@gmail.com>
2022-07-27 12:24:24 -0700

    added collision and planet scaling

--- main.py
+++ main.py
@@ -3,20 +3,28 @@
 
 # import libraries
 from audioop import cross
+from types import NoneType
 import pygame, sys, math
 from pygame.locals import *
+
+# global constants
+# initial velocity constant
+vel_const = 0.6
+#gravitational constant
+g = 50
 
 #Projectile Class
 
 screen_width = 1920
 screen_height= 1080
 class Projectile(pygame.sprite.Sprite):
+    # Initializes projectile at some point on screen with zero velocity
     def __init__(self):
-        self.origin = (20, 500)
+        self.origin = (20, 100)
         super().__init__()
-        self.image = pygame.Surface([10,10])
-        self.image.fill("white")
+        self.image = pygame.transform.scale(pygame.image.load("images/crosshair.png"), (50,50))
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect.center = self.origin
 
         self.xvel = 0
@@ -31,17 +39,16 @@ class Projectile(pygame.sprite.Sprite):
     # Fire sets initial velocity toward crosshair        
     def fire(self):
         #velocity constant
-        self.vel_const = 0.6
         self.fired = True
         mouse_mag = math.sqrt(pygame.mouse.get_pos()[0]**2 + pygame.mouse.get_pos()[1]**2)
-        self.xvel = self.vel_const  * (pygame.mouse.get_pos()[0] - self.origin[0]) / mouse_mag
-        self.yvel = self.vel_const * (pygame.mouse.get_pos()[1] - self.origin[1]) / mouse_mag
+        self.xvel = vel_const  * (pygame.mouse.get_pos()[0] - self.origin[0]) / mouse_mag
+        self.yvel = vel_const * (pygame.mouse.get_pos()[1] - self.origin[1]) / mouse_mag
     
-# Crosshair class
+# Crosshair class - simple crosshair that replaces cursor
 class Crosshair(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load("crosshair.png"), (50,50))
+        self.image = pygame.transform.scale(pygame.image.load("images/crosshair.png"), (50,50))
         self.rect = self.image.get_rect()
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
@@ -50,23 +57,27 @@ class Crosshair(pygame.sprite.Sprite):
 class Body(pygame.sprite.Sprite):
     def __init__(self, image, mass, xpos, ypos):
         super().__init__()
+        # mass -> size of planet
         self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (int((self.image.get_height()) * mass), (self.image.get_width()) * mass))
+
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.mass = mass
         self.rect.center = (xpos, ypos)
-        #gravitational constant
-        self.g = 50
     def update(self, projectile, dt):
         #calculate acceleration due to gravity
         self.xdist = self.rect.center[0] - projectile.rect.center[0]
         self.ydist = self.rect.center[1] - projectile.rect.center[1]
         self.dist = math.sqrt(self.xdist**2 + self.ydist**2)
         #apply gravity to projectile
-        self.ax = self.g * (self.mass * self.xdist) / self.dist**3
-        self.ay = self.g * (self.mass * self.ydist) / self.dist**3
+        self.ax = g * (self.mass * self.xdist) / self.dist**3
+        self.ay = g * (self.mass * self.ydist) / self.dist**3
+        #check if projectile is fired before exerting gravity
         if projectile.fired:
             projectile.xvel += self.ax * dt
             projectile.yvel += self.ay * dt 
+            
 
 # Initialize Objects
 crosshair = Crosshair()
@@ -77,8 +88,8 @@ projectile = Projectile()
 projectile_group = pygame.sprite.Group()
 projectile_group.add(projectile)
 
-planet = Body("crosshair.png", 1, 300, 500)
-planet2 = Body("crosshair.png", 1, 1500, 500)
+planet = Body("images/planet_terra.png", 0.75, 300, 500)
+planet2 = Body("images/planet_jungle.png", 1, 1500, 500)
 body_group = pygame.sprite.Group()
 body_group.add(planet)
 body_group.add(planet2)
@@ -115,5 +126,8 @@ def main():
         projectile_group.draw(DISPLAY)
         projectile_group.update(dt)
         pygame.display.update()
+        # check collision
+        if pygame.sprite.spritecollide(projectile, body_group, False, pygame.sprite.collide_mask):
+            projectile.__init__()
         
 main()
