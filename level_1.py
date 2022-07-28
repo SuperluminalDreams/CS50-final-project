d3f6710149ebf987ae0c4907db82b849eb69712e
SuperluminalDreams <jschubert1016@gmail.com>
2022-07-28 12:11:47 -0700

    added comments

--- level_1.py
+++ level_1.py
@@ -2,11 +2,14 @@
 
 import main, pygame
 
+# create plaentsd
 planet = main.Body("images/planet_terra.png", 0.5, 850, 500)
 planet2 = main.Body("images/planet_jungle.png", 0.65, 1200, 600)
 
+# put them in a group
 body_group = pygame.sprite.Group()
 body_group.add(planet)
 body_group.add(planet2)
 
+# run main with group, desired origin
 main.main(body_group, (200, 500))
